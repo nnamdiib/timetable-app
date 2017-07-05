1ac2326bc6c59f113aaa4e5398c243c080edb942
Nnamdi <nnamdiibs@gmail.com>
2017-07-05 16:40:21 +0100

    Add unit test for class end point

--- api/tests.py
+++ api/tests.py
@@ -73,9 +73,17 @@ class ViewTestCase(TestCase):
                               )
     self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-  def test_api_can_get_a_class(self):
+  def test_api_day_end_point(self):
     response = self.client.get(
                               '/dayend/?code=mat111',
+                              format="json"
+                              )
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertContains(response, self.course.__str__().upper())
+
+  def test_api_class_end_point(self):
+    response = self.client.get(
+                              '/classes/?code=mat111',
                               format="json"
                               )
     self.assertEqual(response.status_code, status.HTTP_200_OK)
