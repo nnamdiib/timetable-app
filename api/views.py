9d3e98ac940892107180372adfda36d7884cade5
Nnamdi <nnamdiibns@gmail.com>
2019-04-26 16:12:58 +0100

    Make views PEP 8 compliant

--- api/views.py
+++ api/views.py
@@ -1,12 +1,15 @@
+import json
+
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
+
 from .serializers import ClassSerializer, DaySerializer
 from .models import Class, Course, Day
 
-import json
 
 def index(request):
     return render(request, 'api/index.html', {})
+
 
 def class_end_point(request):
     queryset = []
@@ -15,19 +18,20 @@ def class_end_point(request):
     if (params):
         if(params[0] == 'all'):
             class_objects = list(Class.objects.all())
-        else:     
-            class_objects = [class_obj for class_obj in Class.objects.filter(course__name__in=params)]
+        else:
+            class_objects = [class_obj for class_obj in Class.objects.filter(
+                course__name__in=params)]
         for class_obj in class_objects:
             course_dict = {
-                            'course': class_obj.course.__str__().upper(),
-                            'time': class_obj.time,
-                            'venue': class_obj.venue,
-                            'day': class_obj.day.__str__()
-                          }
+                'course': class_obj.course.__str__().upper(),
+                'time': class_obj.time,
+                'venue': class_obj.venue,
+                'day': class_obj.day.__str__()
+            }
             queryset.append(course_dict)
     return JsonResponse(queryset, safe=False)
 
-    
+
 def day_end_point(request):
     queryset = {}
     days = list(Day.objects.all())
@@ -36,23 +40,27 @@ def day_end_point(request):
         if(params[0] == 'all'):
             class_objects = list(Class.objects.all())
         else:
-            class_objects = [class_obj for class_obj in Class.objects.filter(course__name__in=params)]
+            class_objects = [class_obj for class_obj in Class.objects.filter(
+                course__name__in=params)]
         for day in days:
             classes_for_day = []
             for class_obj in class_objects:
                 if (class_obj.day == day):
                     course_dict = {
-                                    'course': class_obj.course.__str__().upper(),
-                                    'time': class_obj.time,
-                                    'venue': class_obj.venue,
-                                    'day': class_obj.day.__str__()
-                                  }
+                        'course': class_obj.course.__str__().upper(),
+                        'time': class_obj.time,
+                        'venue': class_obj.venue,
+                        'day': class_obj.day.__str__()
+                    }
                     classes_for_day.append(course_dict)
 
             queryset[day.__str__()] = classes_for_day
     return JsonResponse(queryset)
 
+
 def clear_table(request):
-    # clear the session holding the list of course codes the user had entered previously
+    # clear the session holding the list of course codes the user had entered
+    # previously
     request.session['code_list'] = ''
-    return JsonResponse({'status' : 'success', 'code' : 'The Table Has Been Cleared' })
+    return JsonResponse(
+        {'status': 'success', 'code': 'The Table Has Been Cleared'})
